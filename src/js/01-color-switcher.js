44ffcdedabf4d050096cdf3af87ff9cb5bbe4978
vitalii-borisiuk <vitalii.borisiuk@gmail.com>
2023-01-09 13:59:45 +0200

    Edit part-01 hw-js-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,15 +3,23 @@ let intervalId = null;
 const refs = {
     startBtn: document.querySelector('[data-start]'),
     stopBtn: document.querySelector('[data-stop]'),
-}
+};
+
+
 refs.startBtn.addEventListener('click', changeBg);
 function changeBg() {
-    if (intervalId === null) {
-        intervalId = setInterval(() => document.body.style.backgroundColor = getRandomHexColor(), 1000);   
+    intervalId = setInterval(() => document.body.style.backgroundColor = getRandomHexColor(), 1000);
+    if (intervalId !== null) {
+        document.querySelector('button').disabled = true;
     };
 };
 
-refs.stopBtn.addEventListener('click', () => clearInterval(intervalId));
+refs.stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+    if (intervalId !== null) {
+        document.querySelector('button').disabled = false;
+    }
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,14 @@
+
+const refs = {
+  delay: document.querySelector(''),
+  step: document.querySelector(''),
+  name: document.querySelector(''),
+}
+
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
