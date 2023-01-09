34edbd4c699a07514326acc96ad2c40a6a042766
vitalii-borisiuk <vitalii.borisiuk@gmail.com>
2023-01-09 13:31:15 +0200

    Make task 01 hw-js-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+let intervalId = null;
 
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+refs.startBtn.addEventListener('click', changeBg);
+function changeBg() {
+    if (intervalId === null) {
+        intervalId = setInterval(() => document.body.style.backgroundColor = getRandomHexColor(), 1000);   
+    };
+};
+
+refs.stopBtn.addEventListener('click', () => clearInterval(intervalId));
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
