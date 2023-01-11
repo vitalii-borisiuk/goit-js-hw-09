964831a39545bd1b6c4a9eab709d30f1b4bd75eb
vitalii-borisiuk <vitalii.borisiuk@gmail.com>
2023-01-11 17:42:19 +0200

    Make part-03 js-hw-09

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,22 +1,49 @@
-import { Report } from 'notiflix/build/notiflix-report-aio';
-// import "notiflix-3.2.5.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
 const refs = {
+  form: document.querySelector('.form'),
   delay: document.querySelector('input[name="delay"]'),
   step: document.querySelector('input[name="step"]'),
-  position: document.querySelector('input[name="amount"]'),
-  btn: document.querySelector('button'),
+  amount: document.querySelector('input[name="amount"]'),
+  };
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formDelay = Number(`${refs.delay.value}`);
+  const formStep = Number(`${refs.step.value}`);
+  const formAmount = Number(`${refs.amount.value}`);
+
+  let currentDelay = formDelay;
+  for (let i = 0; i < formAmount; i += 1){
+    if (currentDelay !== 0) {
+      currentDelay += formStep;
+    };
+    createPromise(i + 1, currentDelay);
+  };  
 };
 
-refs.btn.addEventListener('click', createPromise);
-function createPromise() {
-  
-}
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      else {
+        reject({ position, delay });
+      };
+    }, delay);
   });
+
+  promise.then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+};
