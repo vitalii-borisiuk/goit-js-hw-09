1293db8a42377f186e298beeec309debbbb40442
vitalii-borisiuk <vitalii.borisiuk@gmail.com>
2023-01-11 13:18:57 +0200

    Make part 2 hw-js-09

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  minDate: Date.now(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
-
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
     startBtn: document.querySelector('[data-start]'),
@@ -22,30 +11,54 @@ const refs = {
     secs: document.querySelector('[data-seconds]'),
 };
 
-const time = flatpickr(refs.timer, options);
+let selectedDate = null;
+let intervalId = null;
+refs.startBtn.disabled = true;
 
-
-refs.startBtn.addEventListener('click', () => timer.start());
-
-const timer = {
-    start() {
-        // const startTime = Date.now();
-        setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = time.onClose - currentTime;
-            const { hours, mins, secs } = convertMs(deltaTime);
-
-            console.log(`${hours}:${mins}:${secs}`);
-        }, 1000);
-    },
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0].getTime();
+    if (selectedDate < Date.now()) {
+      Notify.failure('Please choose a date in the future.');
+    }
+    refs.startBtn.disabled = false;
+  },
 };
 
-timer.start();
+flatpickr(refs.timer, options);
 
 
 
-function updateTimer({ hours, mins, secs }) {
-    refs.timer.textContent = `${hours}:${mins}:${secs}`;
+
+let object = {};
+
+const timer = () => {
+  intervalId = setInterval(() => {
+    const deltaTime = selectedDate - new Date().getTime();
+    if (deltaTime <= 0) {
+      clearInterval(intervalId);
+    }
+
+    object = convertMs(deltaTime);
+    updateClockFace(object);
+  }, 1000);
+};
+
+
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateClockFace({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${minutes}`;
+  refs.secs.textContent = `${seconds}`;
 }
 
 
@@ -57,13 +70,16 @@ function convertMs(ms) {
     const day = hour * 24;
 
     // Remaining days
-    const days = Math.floor(ms / day);
+    const days = pad(Math.floor(ms / day));
     // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
+    const hours = pad(Math.floor((ms % day) / hour));
     // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
     // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
     return { days, hours, minutes, seconds };
-}
+};
+
+
+refs.startBtn.addEventListener('click', timer);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,18 +11,7 @@ const refs = {
 
 refs.btn.addEventListener('click', createPromise);
 function createPromise() {
-  return new Promise.all  (({ position, delay }) => {
-    const shouldResolve = Math.random() > 0.3;
-    if (resolve) {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    } 
-    else {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    }
-
-  }
-   
- )
+  
 }
 createPromise(2, 1500)
   .then(({ position, delay }) => {
